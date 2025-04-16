5fa07bf5a8dd7814aae6974dc4da83f26ce7b9df
SeanCyan <41523920+SeanCyan@users.noreply.github.com>
2025-04-16 22:30:13 +0100

    carousel amends

--- osr-carousel/src/App.vue
+++ osr-carousel/src/App.vue
@@ -1,14 +1,27 @@
 <script setup>
 import 'vue3-carousel/carousel.css'
 import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+import { ref } from 'vue'
 
-const carouselConfig = {
-  dir: 'ttb',
+const currentSlide = ref(0)
+
+const slideTo = (nextSlide) => (currentSlide.value = nextSlide)
+
+const galleryConfig = {
+  itemsToShow: 1,
   wrapAround: true,
-  itemsToShow:1,
-  snapAlign: 'center',
-  height: '90vh',
-  gap: 5,
+  slideEffect: 'fade',
+  mouseDrag: false,
+  touchDrag: false,
+  height: 320,
+}
+
+const thumbnailsConfig = {
+  height: 80,
+  itemsToShow: 6,
+  wrapAround: true,
+  touchDrag: false,
+  gap: 30,
 }
 
 const images = [
@@ -55,44 +68,100 @@ console.log(images);
 </script>
 
 <template>
-  <Carousel v-bind="carouselConfig">
-    <Slide v-for="img in images" :key="img.id">
-      <img :src="img.url" />
+  <Carousel id="gallery" v-bind="galleryConfig" v-model="currentSlide">
+    <Slide v-for="image in images" :key="image.id">
+      <img :src="image.url" alt="Gallery Image" class="gallery-image" />
+    </Slide>
+  </Carousel>
+
+  <Carousel id="thumbnails" v-bind="thumbnailsConfig" v-model="currentSlide">
+    <Slide v-for="image in images" :key="image.id">
+      <template #default="{ currentIndex, isActive }">
+        <div
+          :class="['thumbnail', { 'is-active': isActive }]"
+          @click="slideTo(currentIndex)"
+        >
+          <img :src="image.url" alt="Thumbnail Image" class="thumbnail-image" />
+        </div>
+      </template>
     </Slide>
 
     <template #addons>
       <Navigation />
-      <Pagination />
     </template>
   </Carousel>
 </template>
 
 <style>
 :root {
-  background-color: #242424;
+  background-color: #cccccc;
 }
-
+#gallery {
+  height:auto;
+}
 .carousel {
-  --vc-pgn-background-color: rgba(255, 255, 255, 0.7);
-  --vc-pgn-active-color: rgba(255, 255, 255, 1);
   --vc-nav-background: rgba(255, 255, 255, 0.7);
   --vc-nav-border-radius: 100%;
-  width:100vw;
-margin: 25px auto 10px;
-}
-
-.carousel__slide {
-  border-radius: 20px;
-  overflow: hidden;
+  
 }
 
 img {
-  border-radius:20px;
-  width: 70vw;
+  border-radius: 8px;
+  width: 100%;
   height: auto;
   object-fit: contain;
+}
+
+
+
+.gallery-image {
+  border-radius: 16px;
+  width: auto;
+  height: auto;
   -webkit-box-shadow: 0px 0px 32px -8px rgba(0,0,0,0.2);
 -moz-box-shadow: 0px 0px 32px -8px rgba(0,0,0,0.2);
 box-shadow: 0px 0px 32px -8px rgba(0,0,0,0.2);
+margin:25px auto;
+}
+
+#thumbnails {
+  margin-top: 10px;
+  width: auto;
+  height: auto;
+
+}
+
+.thumbnail {
+  height: auto;
+  width: 100%;
+  cursor: pointer;
+  opacity: 0.6;
+  transition: opacity 0.3s ease-in-out;
+}
+
+.thumbnail.is-active,
+.thumbnail:hover {
+  opacity: 1;
+}
+.carousel__next,
+.carousel__prev {
+ background: rgba(0, 0, 0, 0.3);
+ border-radius: 50%;
+ width: 40px;
+ height: 40px;
+ color: white;
+ margin: 0 10px;
+}
+@media (max-width: 1024px) {
+  .gallery-image {
+  border-radius: 16px;
+  width: 90vw;
+  height: auto;
+  -webkit-box-shadow: 0px 0px 32px -8px rgba(0,0,0,0.2);
+-moz-box-shadow: 0px 0px 32px -8px rgba(0,0,0,0.2);
+box-shadow: 0px 0px 32px -8px rgba(0,0,0,0.2);
+margin:25px auto;
+}
+
 }
 </style>
